d6ce4f547d31523837979e6a5824079fe014f3c1
swmcguire <swmcguire1@comcast.net>
2022-11-07 18:34:40 -0600

    Can't remember if I pushed my last saves or not - didn't want to lose data

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,17 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
+let
+  lowerCase = "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+  upperCase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z",
+  numeric = "1,2,3,4,5,6,7,8,9,0",
+  special = "!,",#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~"
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
@@ -12,3 +19,34 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+//windows.confirm
+
+//When I click 'generate password'  I get a series of question prompts
+
+
+
+//When prompted for length of the password I can enter 8 - 128
+
+
+
+//When prompted for characther types  confirm - lowercase, uppercase, numeric and special characters
+
+
+//Do you want lowercase?  Y/N
+lowerCase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"
+
+
+//Do you want UpperCase?  Y/N
+upperCase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
+
+
+//Do you want numeric?  Y/N
+numeric = "1,2,3,4,5,6,7,8,9,0"
+
+//Do you want Special Characters?  Y/N
+special = "!,",#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~"
+
+//When I answer each question
