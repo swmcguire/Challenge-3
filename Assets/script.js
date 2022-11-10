e931d94f069bc84c51bc05df5b128d4680a9fe0f
swmcguire <swmcguire1@comcast.net>
2022-11-10 17:42:37 -0600

    linked to function and got everything to run

--- Assets/script.js
+++ Assets/script.js
@@ -16,111 +16,82 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//-------------------NEED TO WRITE THE FUNCTION TO PULL EVERYTHING TOGEHTER--------------------//
-//-------------------ALSO NEED TO SET IT UP SO WHEN I PUSH THE BUTTON IT STARTS THE EVENT, NOT THE PAGE LOAD-------------//
 
 //-------------------set variables for possible Password options --------//
 
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
 var numeric = ["1","2","3","4","5","6","7","8","9","0"];
-
 var special = ["\\","!","\"","\#","$","%","\&", "\'", "(",")","*","+","\,", "-"," ",".", "\/",":", ";", "<","=",">","?","@","[","\]","^","_","\`","{","|","}","~"];
-
+var charLen = " ";
+var pass = " ";
 
 //---------------------Ask for length of password -----------------------//
 
 //-------------------------------------------------------------------------------------HOW CAN I GET TO LOOP BACK IF THE ANSWER IS OUT OF BOUNDS
+function generatePassword(){
 
-var charLen = prompt("Please select length of password (Between 8 and 128 characters)");
-
+charLen = prompt("Please select length of password (Between 8 and 128 characters)");
 if(charLen < 8 || charLen > 128) { ///////  figure out loop here-ish 
-  alert("Please choose number between 8 and 128!")
+  confirm("Please choose number between 8 and 128!")
+  return '';
 } else {
   alert("Great!!! You chose " + charLen + " characters.  Just a couple more questions...");
 }
 
-
 //---------------------Asking if password should have LowerCase (Y/N)------//
-
 var Lower = confirm("Do you want to include LowerCase?")
-
 if (Lower === true) {
   alert("Your password will include LowerCase Letters.");
- } else {("Your password will NOT include LowerCase Letters.");
 }
-
 
 //---------------------Asking if password should have UpperCase (Y/N)------//
-
 var Upper = confirm("Do you want to include UpperCase?")
-
 if (Upper === true) {
   alert("Your password will include UpperCase Letters.");
- } else {("Your password will NOT include UpperCase Letters.");
 }
-
 
 //---------------------Asking if password should have Numbers (Y/N)------//
-
-var Num = confirm("Do you want to include Numbers?")
-
+Num = confirm("Do you want to include Numbers?")
 if (Num === true) {
   alert("Your password will include Numbers.");
- } else {("Your password will NOT include Numbers.");
 }
-
 
 //---------------------Asking if password should have Special Character (Y/N)------//
-
-var specChar = confirm("Do you want to include Special Characters?")
-
+specChar = confirm("Do you want to include Special Characters?")
 if (specChar === true) {
   alert("Your password will include Special Characters.");
- } else {("Your password will NOT include Special Character.");
 }
 
-
-
-//--------define variable characters 
+//-------- If Lower = true, then we add lowerCase to password variable------//
 var characters = "";
-
-//-------- If Lower = true, then we add lowerCase to password variable
-
 if (Lower === true) {
   var characters = lowerCase.join('');
 }
 
-
-//-------- If Upper = true, then we add upperCase to passsword variable 
-
+//-------- If Upper = true, then we add upperCase to passsword variable ------//
 if (Upper === true) {
   var characters = (characters + upperCase.join(''));
 }
 
-
-//-------- If Num = true, then we add numeric to password variable
-
+//-------- If Num = true, then we add numeric to password variable------//
 if (Num === true){
   var characters = (characters + numeric.join(''));
 }
 
-
-//-------- If specChar = true, then we add special to password variable
-
+//-------- If specChar = true, then we add special to password variable------//
 if (specChar === true) {
   var characters = (characters + special.join(''));
 }
-
-
-
-//characters are defined 
 console.log(characters);
 
-//charLen is definited
-console.log(charLen);
+//--------------------------------------- Randomize the password------//
+for (var i = 0; i < charLen; i++){
+  var randomPass = Math.floor(Math.random()*characters.length);
+  pass += characters.substring(randomPass, randomPass +1);
+}
+console.log(pass);
 
+return pass;
 
-//-------- after we have our password variable, we can set a random loop 1 through charLen
+}
--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # Challenge-3
 Challenge 3 - Random Password Generator
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+Due to the ongoing needs of security in the work place I was tasked to create an application that allows employees to generate random passwords based on a set of criteria that they chose.  Employees are given a set of criterion to select for: 
+
+-Length between 8 and 128
+-Includes lowercase letters (Yes or No)
+-Includes uppercase letters (Yes or No)
+-Includes numbers (Yes or No)
+-Includes special characters (Yes or No)
+
+This project was incredibly tricky but it forced me to dig deep and pull many different skills together.  I learned that I am still new to coding and although the code may be a bit sloppy and there is probably a better/ cleaner way to do it I was able to get the task accomplished.
+
+## Installation
+
+No Installation is needed - one can simply navigate to the web address here and begin randomizing their next password. 
+https://swmcguire.github.io/Challenge-3/
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+
+![Password Generator](./Assets/03-javascript-homework-demo.png)
+
+## Credits
+
+https://idp.bootcampspot.com/ui/?requestId=88054420-a92b-406c-a398-9097d021f51a
+https://www.w3schools.com/js/
+
+## License
+
+MIT License
+
+Copyright (c) 2022 swmcguire
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
